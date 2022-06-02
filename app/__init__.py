5693050a6f4e6276988916c710b34b8d72ec0cf9
Guibson Moura <mouraguibson12@gmail.com>
2022-06-02 12:20:22 -0300

    adiconando procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+
 
 
 
 app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/certificado'
-db = SQLAlchemy(app)
+
+
+def create_app():
+    frameWork = Flask(__name__)
+    CORS(frameWork)
+    frameWork.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/certificado'
+    from app.controllers import default
+    return frameWork
+    
 
 
 
-from app.controllers import default
---
+++ app/__pycache__/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/certificado'
+    db = SQLAlchemy(app)
+    return app
+
+
+
+from app.controllers import default
