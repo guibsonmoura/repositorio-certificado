ca75f2f263a33f335193818d7d322fde30350806
Guibson Moura <mouraguibson12@gmail.com>
2022-06-06 16:04:14 -0300

    direncionando arquivos pro mega

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,9 @@
 import os
 from pathlib import Path
+from mega import Mega
 from flask import Flask,request,send_from_directory
 from flask_cors import CORS,cross_origin
 
-
-
-
-app = Flask(__name__)
-CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/certificado'
 
 
 def create_app():
@@ -41,23 +36,46 @@ def create_app():
             return send_from_directory(path_diretorio,nome_arquivo, as_attachment=True)
             # return 'funcionando'
         elif request.method == "POST":
-
+            mega = Mega()
+            login_mega = mega.login(email='mouraguibson12@gmail.com',password='81015021vai')
+            print(f'login: {login_mega}')
             caminho_raiz_aplicacao = os.getcwd()
             
             file = request.files['file']
+            
+            print(file.name)
+            print(file.filename)
+            file.save(f"./{file.filename}")
             req = {
                 "end_point": request.form['end-point'],
-                'nome_arquivo': request.form['nome_arquivo']
+                'nome_arquivo': file.filename
             }
-            path_diretorio = f"{caminho_raiz_aplicacao}\\app\\certificados\\{req['end_point']}"
+            stream_file = file.stream
             
-            diretorio = Path(path_diretorio)
-            diretorio.mkdir(parents=True, exist_ok=True)
-
-            file.save(f"{path_diretorio}\\{req['nome_arquivo']}")
-            
-            return 'arquivo salvo'
-    
+            folder_no = login_mega.find(f"certificados")
+            if folder_no != None:
+                print('pasta certificados existe')
+                #vai gravar na pasta ''certificados''
+                folder = login_mega.find(req['end_point'],exclude_deleted=True)
+                if folder != None:
+                    print('pasta do ent-ponint existe')
+                    file_save = login_mega.find(req['nome_arquivo'],exclude_deleted=True)
+                    if file_save != None:
+                        print('vai fazer upload do arquivo')
+                        login_mega.delete(file_save[0])
+                        login_mega.upload(req['nome_arquivo'], folder[0])
+                        return 'upload do arquivo feito'
+                    else:
+                        login_mega.upload(req['nome_arquivo'], folder[0])
+                        return 'upload do arquivo feito'
+                else: 
+                    print('vai criar pasta com end-point')
+                    login_mega.create_folder(req['end_point'],folder_no[0])
+                    folder = login_mega.find(req['end_point'],exclude_deleted=True)
+                    login_mega.upload(req['nome_arquivo'], folder[0])
+                    return 'upload do arquivo feito'
+            else:
+                return 'pasta certificados não existe'
     return app
     
 
